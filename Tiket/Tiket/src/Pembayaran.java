b5d7f43790c7491a5c18c0b08f24c001eda7afbe
Rifky AlHakim <mrifkyalhakim738@gmail.com>
2024-07-08 05:52:39 +0700

    update

--- Tiket/Tiket/src/Pembayaran.java
+++
@@ -1,7 +0,0 @@
-public class Pembayaran {
-    public boolean prosesPembayaran(double totalPembayaran) {
-        // Logika untuk proses pembayaran, misalnya integrasi dengan sistem pembayaran
-        // Di sini hanya contoh sederhana, bisa diimplementasikan sesuai kebutuhan
-        return true;
-    }
-}
